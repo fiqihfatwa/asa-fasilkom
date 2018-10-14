caaa38ee7f8a449fa18bd7dda33733ec4c48dbc9
Unknown <robinhd300@gmail.com>
2018-10-14 20:43:48 +0700

    change query to get data pegawai

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -10,10 +10,9 @@ class Admin_model extends CI_Model
 
   public function get_data_pegawai()
   {
-    $sql="SELECT * from data_pegawai";
+    $sql="SELECT u.*,p.* from users u,pegawai p where u.group_id='2' and u.kon_id = p.id_pegawai";
     $query = $this->db->query($sql);
     return $query->result();
   }
-
 }
  ?>
