c6743ff5bec7979ab2be851298e0582801ac2df2
Unknown <robinhd300@gmail.com>
2018-10-20 20:53:11 +0700

    surat pernyataan masih kuliah

--- application/controllers/mahasiswa/Kemahasiswaan.php
+++ application/controllers/mahasiswa/Kemahasiswaan.php
@@ -130,10 +130,86 @@ class Kemahasiswaan extends CI_Controller {
 
 	public function surat_pernyataan_masih_kuliah($id_jenis)
 	{
-		$data['title']='surat pernyataan masih aktif kuliah';
 		$data['form_surat']=$this->dbObject->get_form_surat($id_jenis);
+
+		if(isset($_POST['kirim'])){
+
+			$param[0]=$this->session->userdata('nama');
+			$param[1]=$this->session->userdata('nim');
+			//$param[2]=$this->session->userdata('prodi');
+			$param[2]=$_POST['tempat'];
+			$param[3]=$_POST['tglLahir'];
+			$param[4]=$_POST['alamat'];
+			$param[5]=$_POST['hp'];
+			$param[6]=$_POST['email'];
+			$param[7]=$_POST['ThnMsk'];
+			$param[8]=$_POST['semester'];
+			$param[9]=$_POST['rencanaTamat'];
+			$param[10]=$_POST['ips'];
+			$param[11]=$_POST['ipk'];
+			$param[12]=$_POST['keterangan'];
+
+
+						$save_spp = $_FILES['spp']['tmp_name'];
+						$sppfolder = basename($_FILES['spp']['name']);
+						$tipe = $_FILES['spp']['type'];
+						$tipe = explode("/",$tipe);
+						$sp = 'SPP'.'-'.$this->session->userdata('nim');
+						$param[13] = "$sp.$tipe[1]";
+						$sppfolder = "berkas_mahasiswa/documents/$param[13]";
+						move_uploaded_file($save_spp,$sppfolder);
+
+						$save_ktm = $_FILES['ktm']['tmp_name'];
+						$ktm_folder = basename($_FILES['ktm']['name']);
+						$tipe = $_FILES['ktm']['type'];
+						$tipe = explode("/",$tipe);
+						$ktm = 'KTM'.'-'.$this->session->userdata('nim');
+						$param[14] = "$ktm.$tipe[1]";
+						$ktm_folder = "berkas_mahasiswa/documents/$param[14]";
+						move_uploaded_file($save_ktm,$ktm_folder);
+
+						$save_sk = $_FILES['skortu']['tmp_name'];
+						$sk_folder = basename($_FILES['skortu']['name']);
+						$tipe = $_FILES['skortu']['type'];
+						$tipe = explode("/",$tipe);
+						$sk = 'SK-Orangtua'.'-'.$this->session->userdata('nim');
+						$param[15] = "$ktm.$tipe[1]";
+						$sk_folder = "berkas_mahasiswa/documents/$param[15]";
+						move_uploaded_file($save_sk,$sk_folder);
+
+						date_default_timezone_set('Asia/Jakarta');
+						$date = date("Y-m-d");
+						$nim = $this->session->userdata('nim');
+
+						$dataForm = array(
+							'id_jenis' => $id_jenis,
+							'tanggal_masuk' => $date,
+							'nim' => $nim
+						);
+
+						$this->dbObject->create_surat_masuk($dataForm);
+
+						$get_id_isi = $this->dbObject->get_id_isi();
+						foreach ($get_id_isi as $row) {
+							$id_isi = $row->max_id;
+						}
+
+						//var_dump($id_isi);die;
+
+						$i=0;
+						foreach ($data['form_surat'] as $row) {
+							$sql = $this->db->query("INSERT into detail_surat (id_isi,id_surat,keterangan) values ('".$id_isi."','".$row->id_surat."','".$param[$i]."')");
+							++$i;
+						}
+						$this->session->set_flashdata('notif','<div class="alert alert-success">Pengajuan surat berhasil dikirim</div>');
+						redirect('mahasiswa/kemahasiswaan');
+
+		}else{
+		$data['title']='surat pernyataan masih kuliah';
 		$data['id_jenis']=$id_jenis;
 		$this->load->view('mahasiswa/kemahasiswaan/formsurat/pernyataan_masih_kuliah',$data);
+		}
+
 	}
 
 }
--- application/views/mahasiswa/kemahasiswaan/formsurat/pernyataan_masih_kuliah.php
+++ application/views/mahasiswa/kemahasiswaan/formsurat/pernyataan_masih_kuliah.php
@@ -9,18 +9,21 @@
                 </div>
             </div>
               <div class="body" id='body'>
+                <form action="<?php echo base_url()?>mahasiswa/kemahasiswaan/surat_keterangan_aktif_kuliah/<?php echo $id_jenis?>" method="post" enctype="multipart/form-data">
                 <?php foreach ($form_surat as $form): ?>
+                  <h2 class="card-inside-title"><?php echo $form->form_surat?></h2>
                     <div class="row clearfix">
                       <div class="col-sm-12">
-                        <h2 class="card-inside-title"><?php echo $form->form_surat?></h2>
                         <div class="form-group">
                           <div class="form-line">
-                            <input type="<?php echo $form->type ?>" name="<?php echo $form->name ?>" class="form-control" placeholder="<?php echo $form->placeholder ?>">
+                            <input id="<?php echo $form->name ?>" type="<?php echo $form->type ?>" name="<?php echo $form->name ?>" class="form-control" placeholder="<?php echo $form->placeholder ?>" required>
                           </div>
                         </div>
                       </div>
                     </div>
                 <?php endforeach; ?>
+                      <button type="submit" name="kirim" class="btn btn-primary waves-effect">Kirim</button>
+                  </form>
                  </div>
             </div>
       </div>
